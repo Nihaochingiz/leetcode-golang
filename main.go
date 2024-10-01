875249e79f24c9c1597b5f31fce80b6c18f5b6a1
Nihaochingiz <ilyaf1999@gmail.com>
2024-10-01 21:35:22 +0300

    refactor

---
+++ main.go
@@ -0,0 +1,30 @@
+//https://leetcode.com/problems/length-of-last-word/?envType=study-plan-v2&envId=top-interview-150
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+
+
+func lengthOfLastWord(s string) int {
+
+   s = strings.TrimSpace(s)
+   words := strings.Split(s," ")
+   
+
+   if len (words) == 0 {
+	return 0
+   }
+   lastWord := words[len(words) - 1]
+   
+    return len(lastWord)
+} 
+
+func main()  {
+	fmt.Println(lengthOfLastWord("Hello World")) // Output: 5
+    fmt.Println(lengthOfLastWord("   "))          // Output: 0
+
+}
